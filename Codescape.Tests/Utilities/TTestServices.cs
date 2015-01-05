f65f2222d2cc0d4ffa9c9a1e4ed6746f4b8e42c4
Shane Kingsley <shane.kingsley@hq.bcs.org.uk>
2015-01-05 15:21:00 +0000

    Intitial commit

---
+++ Codescape.Tests/Utilities/TTestServices.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Codescape.Utilities.Testing;
+using NUnit.Framework;
+
+namespace Codescape.Tests.Utilities
+{
+	[TestFixture]
+	public class TTestServices
+	{
+		private string filePath = Path.Combine(Path.GetTempPath(), "testingFile.txt");
+
+		[SetUp()]
+		public void UnpackFiles()
+		{
+			TestServices.CreateFile(filePath, "Codescape.Tests.Files.testfile.txt", "Codescape.Tests");
+		}
+
+		[TearDown()]
+		public void TidyUp()
+		{
+			TestServices.DeleteFile(filePath);
+		}
+
+		[Test]
+		public void TestFileWriting()
+		{
+			Assert.True(File.Exists(filePath));
+		}
+	}
+}
---
+++ Codescape.Utilities/Testing/TestServices.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Codescape.Utilities.Testing
+{
+	public class TestServices
+	{
+		public static void CreateFile(string path, string manifestFilename, string overrideAssembly = null)
+		{
+			Assembly a;
+			if (string.IsNullOrEmpty(overrideAssembly))
+				a = Assembly.GetExecutingAssembly();
+			else
+				a = Assembly.Load(overrideAssembly);
+
+			using (Stream s = a.GetManifestResourceStream(manifestFilename))
+			{
+				using (StreamReader sr = new StreamReader(s))
+				{
+					using (StreamWriter sw = File.CreateText(path))
+					{
+						sw.Write(sr.ReadToEnd());
+						sw.Flush();
+					}
+				}
+			}
+		}
+
+		public static void DeleteFile(string path)
+		{
+			if(File.Exists(path))
+				File.Delete(path);
+		}
+	}
+}
